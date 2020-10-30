997a54ee146f6914b74e9e1a3921118e59378e60
ariyaadinatha <ariyaadinatha21@gmail.com>
2020-10-31 01:32:46 +0700

    template file added

--- src/search/routes/web.php
+++ src/search/routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function() {return redirect()->to("/landing");});
+Route::get('/landing', 'PageRequestController@viewLanding');
