a8a61d6d372b469079b04cc2196891bb15384d50
ariyaadinatha <ariyaadinatha21@gmail.com>
2020-11-16 10:03:09 +0700

    searching berhasil
    searching berhasil, walaupun ada bad code (maklum ane masih cupu), sql bakal dibackup biar bisa dideploy langsung, tabel term lagi dibuat, about us beres

--- src/resources/views/about/aboutIndex.blade.php
+++ src/resources/views/about/aboutIndex.blade.php
@@ -4,19 +4,48 @@
 <div class="container">
 <div class="jumbotron-fluid pt-3">
   <h1 class="display-4">What is Search Engine?</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead">
+  A search engine is a program that searches for and identifies items in a list of documents that correspond to keywords or characters specified by the user, used especially for finding particular documents
+versi diubah
+  </p>
   <hr class="my-5">
 
 
   <h1 class="display-5">How to Use</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p> 
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead text-justify">
+    <table>
+      <tr>
+        <td> 1.
+        <td> Halaman Search digunakan untuk memasukkan query yang ingin dicari dalam bahasa inggris. 
+      <tr>
+        <td> 2.
+        <td> Halaman Upload digunakan untuk memasukkan dokumen dari pengguna ke dalam sistem.
+      <tr>
+        <td> 3.
+        <td> Halaman berisikan seluruh dokumen yang sudah diupload sebelumnya.
+      <tr>
+        <td> 4.
+        <td> Halaman search, pengguna cukup memasukkan query dalam bahasa inggris, daftar dokumen kemudian akan muncul dari yang 
+              paling mirip.
+      <tr>
+        <td> 5.
+        <td> Halaman upload, pengguna perlu memasukkan judul dokumen, deskripsi dokumen,
+      <tr>
+        <td>
+        <td> file dokumen itu sendiri, dan sebuah foto yang mewakili dokumen tersebut. 
+    </table>
+  </p>
   <hr class="my-5">
 
   <h1 class="display-5">About Us</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead">
+  Ini adalah sebuah search engine yang dibuat oleh ASYNC kelompok 14 yang terdiri dari <br>
+  <br> Ariya Adinatha (13519048) K04
+  <br> Wisnu Aditya Samiadji (13519093) K01
+  <br> Hokki Suwanda (13519143) K03 <br>
+  <br>  guna memenuhi tugas besar mata kuliah IF2123 Aljabar Linier dan Geometri
+  
+  </p>
   <hr class="my-5">
 
 </div>
